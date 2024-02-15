d96924c5f117dd5b7b04c6de7828ace768715efb
manibalaji003 <manibalaji22003@gmail.com>
2024-02-15 16:29:11 +0530

    usestate for table data is comlpeted

--- Frontend/src/Components/table.jsx
+++ Frontend/src/Components/table.jsx
@@ -1,65 +1,90 @@
 import React ,{useState} from 'react'
 import {Container,Table,Button} from 'react-bootstrap'
+import axios from 'axios'
 
 const arr=['a','ab','abc','abcd','abcde','None']
 const subject =['Engineering maths','Technical English',"Physics","Chemistry"]
 const numcount=[10,20,30,40]
 
 
-function Dropdown(a){
-    return a.map((option, index) => (
-      <option key={index} value={option}>{option}</option>
-    ))
-  }
-  
-  function disSub() {
-    return subject.map((subj, i) => (
-        <tr key={i}>
-            <td>{i + 1}</td>
-            <td>{subj}</td>
-            <td>
-                <select name={`Batch${i+1}-${subj}`}>
-                  {Dropdown(arr)}
-                </select>
-            </td>
-            <td>
-            <select name={`Batch${i+1}-${subj}-count`}>
-            {Dropdown(numcount)}
-                </select>
-            </td>
-            <td>
-                <select name={`Batch${i+1}-${subj}`}>
-                  {Dropdown(arr)}
-                </select>
-            </td>
-            <td>
-            <select name={`Batch${i+1}-${subj}-count`}>
-            {Dropdown(numcount)}
-                </select>
-              
-            </td>
-            <td>
-                <select name={`Batch${i+1}-${subj}`}>
-                  {Dropdown(arr)}
-                </select>
-            </td>
-            <td>
-            <select name={`Batch${i+1}-${subj}-count`}>
-            {Dropdown(numcount)}
-                </select>
-            </td>
-        </tr>
-    ));
-  }
 
 
 const Tablee = () => {
-    const [tableData, setTableData]=useState(' ')
+    const [tableData, setTableData]=useState({ })
+
+
+
+
+
+    function Dropdown(a){
+        return a.map((option, index) => (
+          <option key={index} value={option}>{option}</option>
+        ))
+      }
+      
+      function disSub() {
+        return subject.map((subj, i) => (
+            <tr key={i}>
+                <td>{i + 1}</td>
+                <td>{subj}</td>
+                <td>
+                    <select name={`Batch${i+1}-${subj}`} onChange={handlingSubjData}>
+                      {Dropdown(arr)}
+                    </select>
+                </td>
+                <td>
+                <select name={`Batch${i+1}-${subj}-count`} onChange={handlingSubjData}>
+                {Dropdown(numcount)}
+                    </select>
+                </td>
+                <td>
+                    <select name={`Batch${i+1}-${subj}`} onChange={handlingSubjData}>
+                      {Dropdown(arr)}
+                    </select>
+                </td>
+                <td>
+                <select name={`Batch${i+1}-${subj}-count`} onChange={handlingSubjData}>
+                {Dropdown(numcount)}
+                    </select>
+                  
+                </td>
+                <td>
+                    <select name={`Batch${i+1}-${subj}`} onChange={handlingSubjData}>
+                      {Dropdown(arr)}
+                    </select>
+                </td>
+                <td>
+                <select name={`Batch${i+1}-${subj}-count`} onChange={handlingSubjData}>
+                {Dropdown(numcount)}
+                    </select>
+                </td>
+            </tr>
+        ));
+      }
+
+
+
+    const handlingSubjData = (e) =>{
+        const {name, value}=e.target;
+        setTableData(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
     
+    const formtablesubmit = async (e) =>{
+        e.preventDefault();
+        try{
+            const response =axios.post("https:/localhost:5000",tableData);
+            console.log("formed submitted sucessfully",response.data);
+        }catch{
+            console.log("error occured");
+        }
+    }
   return (
     <Container>
-                <form>
-                <Table >
+                <form  onSubmit={formtablesubmit}>
+                <Table  >
                     <thead >
                     <tr>
                     <th>S.NO</th>
