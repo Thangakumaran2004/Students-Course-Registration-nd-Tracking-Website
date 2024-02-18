1216cad3e027fe68d02d8763fe1034e14250f7ae
Thangakumaran2004 <thangakumarans2004@gmail.com>
2024-02-18 11:52:14 +0530

    Changed the way response go from backend to frontend in adminloginroute and studentloginroute, changed the database a litte bit

--- Backend/Controllers/studentLoginPageController.js
+++ Backend/Controllers/studentLoginPageController.js
@@ -2,7 +2,7 @@ const db = require('../databaseConnection');
 
 
 const checkStudent = async (username, password)=>{
-    let checkStudentQuery = `SELECT cast(regno as char) as RegNo, cast(dob as char) as DateOfBirth FROM Students WHERE regno = ?`;
+    let checkStudentQuery = `SELECT cast(regno as char) as RegNo, cast(dob as char) as DateOfBirth, cast(dept as char) as dept FROM Students WHERE regno = ?`;
     return new Promise((resolve,reject)=>{
         db.query(checkStudentQuery,username,(err,res)=>{
             if(err){
@@ -13,7 +13,9 @@ const checkStudent = async (username, password)=>{
                 if(res.length == 1){
                     let response = {
                                         status: "Student found in database",
-                                        password: res[0].DateOfBirth
+                                        password: res[0].DateOfBirth,
+                                        regno: res[0].RegNo,
+                                        dept: res[0].dept
                                    };
                     resolve(response);
 
--- Frontend/src/Pages/studentLoginPage.jsx
+++ Frontend/src/Pages/studentLoginPage.jsx
@@ -36,7 +36,7 @@ const StudentLoginpage = () => {
     
     try{
       let response = await axios.post('http://localhost:5000/studentLogin', stddetails) ;
-      console.log(response);
+      console.log(response.data);
     }catch(e){
       console.log("Error hapened while fetching response from studentLogin API",e);
     }
