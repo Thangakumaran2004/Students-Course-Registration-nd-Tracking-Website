a4f63e1ba71123f8eee3c90a91bbe68afa2cefa8
Thangakumaran2004 <thangakumarans2004@gmail.com>
2024-02-27 14:27:10 +0530

    Corrected the bugs in routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,33 @@
 import React from 'react';
-// import Studentpage from './Pages/Student/studentMainPage';
-// import AdminLoginpage from './Pages/Admin/adminLoginPage';
+ import Studentpage from './Pages/Student/studentMainPage';
+ import AdminLoginpage from './Pages/Admin/adminLoginPage';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-// import StudentLoginpage from './Pages/Student/studentLoginPage';
-// import Adminmainpage from './Pages/Admin/adminMainPage';
-// import Landingpage from './Pages/LandingPage';
-// import Adminstudent from './Pages/Admin/adminStudents';
-// import Adminaddcourse from './Pages/Admin/adminCourse';
+ import StudentLoginpage from './Pages/Student/studentLoginPage';
+ import Adminmainpage from './Pages/Admin/adminMainPage';
+ import Landingpage from './Pages/LandingPage';
+ import Adminstudent from './Pages/Admin/adminStudents';
+ import Adminaddcourse from './Pages/Admin/adminCourse';
 import Adminfaculty from './Pages/Admin/adminfaculty';
-// import Adminaddfaculty from './Pages/Admin/adminfaculty';
-// import Adminfaculty from './Pages/Admin/adminfaculty';
-// import AdminDeleteFaculty from './Pages/Admin/adminDeleteFaculty';
+ import Adminaddfaculty from './Pages/Admin/adminfaculty';
+
+ import AdminDeleteFaculty from './Pages/Admin/adminDeleteFaculty';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Adminfaculty />} />
-          {/* <Route path='/adminlogin' element={<AdminLoginpage />} />
           <Route path='/' element={<Landingpage />} />
+           <Route path='/adminlogin' element={<AdminLoginpage />} />
+          
           <Route path='/studentpage' element={<Studentpage />} />
           <Route path='/studentlogin' element={<StudentLoginpage />} />
-          <Route path='/adminpage' element={<Adminmainpage />}>
-            <Route path='addstudent' element={<Adminstudent />} />
-            <Route path='faculty' element={<Adminfaculty />}>
-              <Route path='addfaculty' element={<Adminaddfaculty />} />
-            </Route>
-            <Route path='allotcourse' element={<Adminaddcourse />} />
-          </Route> */}
+          <Route path='/adminpage' element={<Adminmainpage />}/>
+            <Route path='/adminpage/student' element={<Adminstudent />} />
+            <Route path='adminpage/faculty' element={<Adminfaculty/>}/>
+            <Route path='/adminpage/addfaculty' element={<Adminaddfaculty />} />
+            <Route path='/adminpage/allotcourse' element={<Adminaddcourse />} />
+          
         </Routes>
       </BrowserRouter>
     </>
