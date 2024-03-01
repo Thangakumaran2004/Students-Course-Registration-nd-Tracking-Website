a40da457fa769e7f648e103c2715059913e61a22
Thangakumaran2004 <thangakumarans2004@gmail.com>
2024-03-01 23:10:21 +0530

    summa

--- Backend/Routes/adminAllotFacultyRoute.js
+++ Backend/Routes/adminAllotFacultyRoute.js
@@ -4,17 +4,17 @@ const {addDataToDB} = require('../Controllers/adminAllotFacultyController');
 
 router.use(express.json());
 
-
-
 router.post('/',async (req,res)=>{
     const frontendData = req.body;
     console.log("The frontend data is",frontendData);
-    const response = await addDataToDB(frontendData,'ece',3,2025);
+    const deptSemBatchDetails = JSON.parse(frontendData.deptSemBatch);
+    const {dept,year,semester,batch} = deptSemBatchDetails;
+    console.log("the destructured deptSemBatch details is,",dept,year,semester,batch);
+    const response = await addDataToDB(frontendData,dept,semester,batch);
     if(response=="Server Busy"){
         let result = {
             addAllotedFacultiesToDBStatus : "Server Busy"
         }
-
     }else{
         let result = {
             addAllotedFacultiesToDBStatus : "Successfully added allotted faculties to database"
@@ -23,7 +23,5 @@ router.post('/',async (req,res)=>{
     }
 
 });
-
-
 
 module.exports = router;
